ac0f255955ee32fb38058bbc91432203416690ed
javiza <jonathan@gmail.com>
2024-05-04 15:33:08 -0400

    tercer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.vuelos.app',
+  appName: 'gestion_vuelos',
+  webDir: 'www'
+};
+
+export default config;
