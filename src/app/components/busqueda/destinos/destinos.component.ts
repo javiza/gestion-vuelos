f38471f9d6835561cf53c5928db4353bb4fb119c
javiza <jonathan@gmail.com>
2024-05-07 23:13:02 -0400

    trabajos con destinos component, se asigna item, falta que se muestren los iconos en pantalla

--- src/app/components/busqueda/destinos/destinos.component.ts
+++ src/app/components/busqueda/destinos/destinos.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { IonRow, IonCol, IonIcon, IonImg, IonList, IonItem, IonButton, IonContent, IonCard} from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-destinos',
   templateUrl: './destinos.component.html',
   styleUrls: ['./destinos.component.scss'],
   standalone: true,
+  imports:[ IonicModule, CommonModule, FormsModule,IonRow, IonCol, IonIcon, IonImg, IonList, IonItem, IonButton, IonContent, IonCard
+
+  ]
 })
 export class DestinosComponent  implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
+  handleIconClick(icon: string) {
+    console.log(`Se hizo clic en el icono ${icon}`);
+    // Agrega aquí la lógica para manejar el clic en el icono
+  }
 
 }
